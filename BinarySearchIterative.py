3de5ba547b883db8a37d0403b09edcf661f34015
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-01-10 21:42:18 +0530

    Binary Search

---
+++ BinarySearchIterative.py
@@ -0,0 +1,23 @@
+#Iterative Method
+def Bsearch(arr,s,left,right):
+    while left<=right:
+        mid=(left+right)//2
+        if arr[mid]==s:
+            return mid+1
+        elif arr[mid]<s:
+            left=mid+1
+        else:
+            right=mid-1
+    return -1
+
+#Driver Code
+#using already sorted array for Binary search
+arr=[2,5,7,8,9,10,12,15,17,19,23,45,56,58,67,69,70,73,76,78,79,89,98,99,102,134,156]
+s=7
+left=0
+right=len(arr)-1
+res=(Bsearch(arr,s,left,right))
+if res!=-1:
+    print("Element is present at: ",res)
+else:
+    print("Element not found")
