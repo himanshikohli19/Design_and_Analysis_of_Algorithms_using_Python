cab511374edda3791b0aeb4e4014846ecc17c092
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-01-24 13:46:49 +0530

    Activity Selection

--- ActivitySelection_greedy.py
+++ ActivitySelection_greedy.py
@@ -3,10 +3,11 @@ are already sorted according to their finish time"""
 
 """Prints a maximum set of activities that can be done by a
 single person, one at a time"""
-# n --> Total number of activities
-# s[]--> An array that contains start time of all activities
-# f[] --> An array that contains finish time of all activities
-
+'''
+n --> Total number of activities
+s[]--> An array that contains start time of all activities
+f[] --> An array that contains finish time of all activities
+'''
 def printMaxActivities(s, f ):
 	n = len(f)
 	print("The following activities are selected")
