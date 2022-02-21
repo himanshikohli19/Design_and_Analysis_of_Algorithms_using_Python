a810ed9babc662580cd34c1323c037be427ae806
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-02-21 22:23:01 +0530

    Selection Sort

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,13 +1,13 @@
 #SELECTION SORT
 #TIME COMPLEXITY: O(N^2)
-#
+
 def SelectionSort(arr):
     for i in range(len(arr)):
         min_index=i
         for j in range(i+1, len(arr)):
             if arr[min_index] > arr[j]:
                 min_index=j
-                arr[i], arr[j] = arr[j], arr[i]
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     
     return arr
 
@@ -15,4 +15,4 @@ def SelectionSort(arr):
 if __name__ == '__main__':
     arr=[64, 25, 12, 22, 10, 8, 11]
     print('Original Array: ', arr)
-    print('Sorted Array: ',SelectionSort(arr))
+    print('Sorted Array: ', SelectionSort(arr))
