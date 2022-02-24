9c59cb12f3f496537fbd6e380d83c6b5d397fc9b
himanshikohli19 <himanshi.kohli19@gmail.com>
2022-02-24 21:01:04 +0530

    Merge Sort

--- MergeSort.py
+++ MergeSort.py
@@ -1,56 +1,41 @@
 # MergeSort in Python
-
+#Time Complexity: O(N*log(N))
 
 def mergeSort(array):
     if len(array) > 1:
-
-        #  r is the point where the array is divided into two subarrays
-        r = len(array)//2
-        L = array[:r]
-        M = array[r:]
-        # Sort the two halves
-        mergeSort(L)
-        mergeSort(M)
+        Mid = len(array)//2
+        Left_arr = array[:Mid]
+        Right_arr = array[Mid:]
+        
+        mergeSort(Left_arr)
+        mergeSort(Right_arr)
 
         i = j = k = 0
 
-        # Until we reach either end of either L or M, pick larger among
-        # elements L and M and place them in the correct position at A[p..r]
-        while i < len(L) and j < len(M):
-            if L[i] < M[j]:
-                array[k] = L[i]
+        while i < len(Left_arr) and j < len(Right_arr):
+            if Left_arr[i] < Right_arr[j]:
+                array[k] = Left_arr[i]
                 i += 1
             else:
-                array[k] = M[j]
+                array[k] = Right_arr[j]
                 j += 1
             k += 1
 
-        # When we run out of elements in either L or M,
-        # pick up the remaining elements and put in A[p..r]
-        while i < len(L):
-            array[k] = L[i]
+        while i < len(Left_arr):
+            array[k] = Left_arr[i]
             i += 1
             k += 1
 
-        while j < len(M):
-            array[k] = M[j]
+        while j < len(Right_arr):
+            array[k] = Right_arr[j]
             j += 1
             k += 1
-
-
-# Print the array
-def printList(array):
-    for i in range(len(array)):
-        print(array[i], end=" ")
-    print()
 
 
 # Driver program
 if __name__ == '__main__':
     array = [6, 5, 12, 10, 9, 1,16, 25, 2, 100, 9, 16]
-    print("Original Array: ", array)
-    
+    print("Original Array: ", array) 
     mergeSort(array)
-
-    print("Sorted array is: ")
-    printList(array)
+    print("Sorted array is: ", array)
+    
--- improved_SelectionSort.py
+++ improved_SelectionSort.py
@@ -2,3 +2,18 @@
 Selection Sort - Improved
 Idea: keep track of both maximum and minimum and array becomes sorted from both ends.
 '''
+def improvedSelectionSort(arr):
+    for i in range(len(arr)):
+        min_index=i
+        for j in range(i+1, len(arr)):
+            if arr[min_index] > arr[j]:
+                min_index=j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [98, 12, 56, 78, 89, 24, 47, 66, 25, 6, 10]
+    print('Original Array: ', arr)
+    print('Sorted Array: ', improvedSelectionSort(arr))
